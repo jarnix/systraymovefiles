9f86c350394cedf8430541da409965c8c66c4394
Julien Ricard <jarnix@jarnix.com>
2019-03-18 22:38:36 +0100

    wip

--- main.go
+++ main.go
@@ -3,17 +3,21 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"time"
+	"log"
 
 	"github.com/getlantern/systray"
+	"gopkg.in/yaml.v2"
 )
 
 // Config contains our configuration
 type Config struct {
-	Item struct {
-		From string `yaml:"from"`
-		To   string `yaml:"dest"`
-	}
+	Item Item
+}
+
+type Item struct {
+	Extension string `yaml:"extension"`
+	From      string `yaml:"from"`
+	Dest      string `yaml:"dest"`
 }
 
 func main() {
@@ -25,12 +29,40 @@ func onReady() {
 	systray.SetTitle("Systray move files")
 	systray.SetTooltip("Look at me, I'm a tooltip!")
 
-	go func() {
-		for {
+	dat, err := ioutil.ReadFile("config.yml")
+	if err != nil {
+		panic(err)
+	}
 
-			time.Sleep(1 * time.Second)
-		}
-	}()
+	m1 := Item{
+		Extension: "torrent",
+		From:      "toto",
+		Dest:      "tutu",
+	}
+
+	m := Config{Item: m1}
+
+	d, err := yaml.Marshal(&m)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	fmt.Printf("--- m dump:\n%s\n\n", string(d))
+
+	config := Config{}
+	err = yaml.Unmarshal([]byte(dat), &config)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	fmt.Printf("--- t:\n%v\n\n", config)
+
+	/*
+		go func() {
+			for {
+
+				time.Sleep(1 * time.Second)
+			}
+		}()
+	*/
 
 }
 
